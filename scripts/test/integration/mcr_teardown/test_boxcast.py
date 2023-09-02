6dd01f73e42a3a6d5e08a5e25e4c1384e0308fe0
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-09-02 17:25:21 -0400

    Test downloading captions from BoxCast

---
+++ scripts/test/integration/mcr_teardown/test_boxcast.py
@@ -0,0 +1,111 @@
+import os
+import unittest
+from datetime import datetime
+from pathlib import Path
+from unittest.mock import Mock
+
+import mcr_teardown.tasks as tasks
+from autochecklist.messenger import Messenger
+from mcr_teardown.boxcast import BoxCastClient, BoxCastClientFactory
+from mcr_teardown.config import McrTeardownConfig
+from mcr_teardown.credentials import Credential, CredentialStore
+
+EVENT_ID = "oajqcyzetaazjvduyqz5"
+BOXCAST_TEST_USERNAME = "tech@riversedge.life"
+
+
+class BoxCastTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.error_message = ""
+        messenger = Mock(spec=Messenger)
+        credential_store = CredentialStore(messenger)
+        boxcast_username = credential_store.get(
+            Credential.BOXCAST_USERNAME, force_user_input=False
+        )
+        if boxcast_username != BOXCAST_TEST_USERNAME:
+            cls.error_message = f"Expected the BoxCast username to be '{BOXCAST_TEST_USERNAME}' but found username '{boxcast_username}'. Run python check_credentials.py --force-input to set the correct credentials."
+            return
+        try:
+            BoxCastClientFactory(
+                messenger=Mock(),
+                credential_store=credential_store,
+                headless=True,
+                lazy_login=False,
+                log_directory=None,
+                log_file_name=None,
+            )
+        except Exception as e:
+            raise RuntimeError(
+                "Failed to log in to BoxCast. Run python check_credentials.py to set the correct credentials."
+            ) from e
+
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        cls.home_dir = (
+            Path(__file__).parent.joinpath("test_home").joinpath(f"{timestamp}_home")
+        )
+        cls.home_dir.mkdir(exist_ok=True, parents=True)
+
+    def setUp(self) -> None:
+        if self.error_message:
+            self.fail(self.error_message)
+
+    def test_captions(self):
+        messenger = Mock(spec=Messenger)
+        log_problem_mock = Mock()
+        messenger.log_problem = log_problem_mock
+        credential_store = CredentialStore(messenger)
+        boxcast_client = BoxCastClient(
+            messenger=messenger, credential_store=credential_store
+        )
+        boxcast_client_factory = Mock(spec=["get_client"])
+        boxcast_client_factory.get_client.return_value = boxcast_client  # type: ignore
+        config = McrTeardownConfig(
+            home_dir=self.home_dir,
+            downloads_dir=Path(os.environ["USERPROFILE"]).joinpath("Downloads"),
+            boxcast_event_id=EVENT_ID,
+        )
+
+        with self.subTest(msg="download_captions"):
+            tasks.download_captions(
+                boxcast_client_factory=boxcast_client_factory,
+                config=config,
+                messenger=messenger,
+            )
+            expected_file = Path(__file__).parent.joinpath("captions.vtt")
+            with open(expected_file, mode="r", encoding="utf-8") as f:
+                expected_captions = f.read()
+            with open(config.original_captions_path, mode="r", encoding="utf-8") as f:
+                actual_captions = f.read()
+            self.assertEqual(expected_captions, actual_captions)
+            log_problem_mock.assert_not_called()
+
+        # Make sure that warnings or errors from the first sub-test don't
+        # affect the next one
+        messenger.reset_mock()
+
+        with self.subTest(msg="copy_captions_to_without_worship"):
+            tasks.copy_captions_to_without_worship(config=config, messenger=messenger)
+            with open(config.original_captions_path, mode="r", encoding="utf-8") as f:
+                original_captions = f.read()
+            with open(
+                config.captions_without_worship_path, mode="r", encoding="utf-8"
+            ) as f:
+                captions_without_worship = f.read()
+            self.assertEqual(original_captions, captions_without_worship)
+            log_problem_mock.assert_not_called()
+            # TODO: Check that file is read-only?
+
+        messenger.reset_mock()
+
+        with self.subTest(msg="copy_captions_to_final"):
+            tasks.copy_captions_to_final(config=config, messenger=messenger)
+            with open(
+                config.captions_without_worship_path, mode="r", encoding="utf-8"
+            ) as f:
+                captions_without_worship = f.read()
+            with open(config.final_captions_path, mode="r", encoding="utf-8") as f:
+                final_captions = f.read()
+            self.assertEqual(captions_without_worship, final_captions)
+            log_problem_mock.assert_not_called()
+            # TODO: Check that the file is read-only?
