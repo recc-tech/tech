1a4d24f908251cd61502224c5e1542b672f3dc0f
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-12-09 17:26:08 -0500

    Improve update script

--- scripts/run_scripts_update.bat
+++ scripts/run_scripts_update.bat
@@ -4,7 +4,10 @@
 :: You need to pass the /D flag because the MCR computer uses the D:/ drive.
 CD /D %~dp0
 
+git stash --include-untracked
+git switch main
 git pull
+
 pip install --upgrade -r requirements.txt
 
 ECHO(
