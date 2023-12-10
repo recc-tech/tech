8ad4f913218548d765a21adf3bf877fb6e7dbdbc
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-12-10 09:41:37 -0500

    Add application/mp4 MIME type for downloading PCO assets

--- scripts/mcr_setup/tasks.py
+++ scripts/mcr_setup/tasks.py
@@ -27,6 +27,7 @@ def download_assets(
     plan = client.find_plan_by_date(today)
     attachments = client.find_attachments(plan.id)
 
+    print(attachments)
     kids_video, sermon_notes, other_images, other_videos = _classify_attachments(
         attachments
     )
@@ -95,8 +96,9 @@ def download_assets(
             archived_path.unlink()
 
 
-_VIDEO_CONTENT_TYPES = {"video/av1", "video/mp4", "video/mpeg", "video/quicktime"}
-_KIDS_VIDEO_FILENAME_REGEX = re.compile(r"^Kids.*", flags=re.IGNORECASE)
+# TODO: Update these lists
+_VIDEO_CONTENT_TYPES = {"video/av1", "video/mp4", "video/mpeg", "video/quicktime", "application/mp4"}
+_KIDS_VIDEO_FILENAME_REGEX = re.compile(r"^kids.*", flags=re.IGNORECASE)
 _DOCX_CONTENT_TYPE = (
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 )
