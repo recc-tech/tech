dd3cb09841d3e8340c1a5262cb2fb14b4198bf58
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-12-18 15:19:43 -0500

    Improve logging and exception handling when downloading assets

--- scripts/mcr_setup/tasks.py
+++ scripts/mcr_setup/tasks.py
@@ -2,9 +2,10 @@ import asyncio
 import filecmp
 import re
 import shutil
+import traceback
 from datetime import datetime
 from pathlib import Path
-from typing import List, Set, Tuple
+from typing import List, Optional, Set, Tuple
 
 from autochecklist import Messenger, ProblemLevel, TaskStatus
 from common import Attachment, PlanningCenterClient
@@ -62,35 +63,38 @@ def download_assets(
     plan = client.find_plan_by_date(today)
     attachments = client.find_attachments(plan.id)
 
-    kids_video, sermon_notes, other_images, other_videos = _classify_attachments(
-        attachments
+    (
+        kids_video,
+        sermon_notes,
+        other_images,
+        other_videos,
+        unknown_attachments,
+    ) = _classify_attachments(attachments)
+    messenger.log_debug(
+        f"{len(attachments)} attachments found on PCO.\n- Kids video: {kids_video}\n- Sermon notes: {sermon_notes}\n- Other images: {other_images}\n- Other videos: {other_videos}\n- Unknown: {unknown_attachments}"
     )
 
     config.assets_by_service_dir.mkdir(exist_ok=True, parents=True)
 
     # Prepare for downloads
+    # IMPORTANT: the kids video must be the first thing in the downloads list
     messenger.log_status(TaskStatus.RUNNING, "Preparing for download.")
-    downloads: List[Tuple[Attachment, Path]] = []
-    archived_files: List[Tuple[Path, Path]] = []
+    downloads: List[Tuple[Attachment, Path, Optional[Path]]] = []
     if len(kids_video) != 1:
         raise ValueError(
             f"Found {len(kids_video)} attachments that look like the Kids Connection video."
         )
     kids_video = _any(kids_video)
     kids_video_path = config.assets_by_service_dir.joinpath(kids_video.filename)
-    downloads.append((kids_video, kids_video_path))
-    if len(sermon_notes) == 0:
+    downloads.append((kids_video, kids_video_path, None))
+    if len(sermon_notes) != 1:
         messenger.log_problem(
-            ProblemLevel.WARN, "Found 0 attachments that look like the sermon notes."
+            ProblemLevel.WARN,
+            f"Found {len(sermon_notes)} attachments that look like sermon notes.",
         )
-    elif len(sermon_notes) == 1:
-        sermon_notes = _any(sermon_notes)
-        sermon_notes_path = config.assets_by_service_dir.joinpath(sermon_notes.filename)
-        downloads.append((sermon_notes, sermon_notes_path))
-    else:
-        raise ValueError(
-            f"Found {len(sermon_notes)} attachments that look like sermon notes."
-        )
+    for sn in sermon_notes:
+        sermon_notes_path = config.assets_by_service_dir.joinpath(sn.filename)
+        downloads.append((sn, sermon_notes_path, None))
     for img in other_images:
         img_path = config.assets_by_type_images_dir.joinpath(img.filename)
         if img_path.exists():
@@ -99,45 +103,58 @@ def download_assets(
             )
             # Microseconds --> milliseconds
             timestamp = timestamp[:-3]
-            archived_img_name = (
-                f"{img_path.stem} (archived {timestamp}){img_path.suffix}"
-            )
-            archived_img_path = config.assets_by_type_archive_dir.joinpath(
-                archived_img_name
-            )
-            archived_files.append((img_path, archived_img_path))
+            name = f"{img_path.stem} (archived {timestamp}){img_path.suffix}"
+            archived_img_path = config.assets_by_type_archive_dir.joinpath(name)
             # Copy instead of moving in case an error occurs before the new
             # file is downloaded
             shutil.copyfile(img_path, archived_img_path)
-        downloads.append((img, img_path))
+        else:
+            archived_img_path = None
+        downloads.append((img, img_path, archived_img_path))
     for vid in other_videos:
         vid_path = config.assets_by_type_videos_dir.joinpath(vid.filename)
         # Assume the existing video is already correct
         if not vid_path.exists():
-            downloads.append((vid, vid_path))
+            downloads.append((vid, vid_path, None))
 
     messenger.log_status(TaskStatus.RUNNING, "Downloading new assets.")
-    try:
-        asyncio.run(client.download_attachments(downloads, cancellation_token))
-    except BaseException:
-        # The Planning Center client should take care of deleting
-        # partially-downloaded assets on error.
-        # Don't bother deleting assets that were successfully downloaded: the
-        # user can easily take care of those if needed.
-        for live_path, archived_path in archived_files:
-            shutil.move(archived_path, live_path)
-            archived_path.unlink(missing_ok=True)
-        raise
-
-    # If new and existing files were the same, no need to keep archived copy
-    if len(archived_files) > 0:
-        messenger.log_status(
-            TaskStatus.RUNNING, "Comparing new assets with existing ones."
+    results = asyncio.run(
+        client.download_attachments(
+            downloads=[(a, p) for (a, p, _) in downloads],
+            cancellation_token=cancellation_token,
         )
-    for live_path, archived_path in archived_files:
-        same = filecmp.cmp(live_path, archived_path, shallow=False)
-        if same:
-            archived_path.unlink()
+    )
+
+    messenger.log_status(TaskStatus.RUNNING, "Checking downloaded assets.")
+    for i, (_, live_path, archive_path) in enumerate(downloads):
+        if i == 0:
+            # Deal with the kids video last so an exception can be thrown
+            continue
+        if i >= len(results):
+            break
+        try:
+            if results[i] is not None:
+                messenger.log_problem(
+                    ProblemLevel.WARN,
+                    f"Download to '{live_path.as_posix()}' failed: {results[i]}",
+                )
+            if results[i] is not None and archive_path is not None:
+                # Restore from archive
+                shutil.move(src=archive_path, dst=live_path)
+            elif results[i] is None and archive_path is not None:
+                same = filecmp.cmp(live_path, archive_path, shallow=False)
+                if same:
+                    # No need to keep archived copy
+                    archive_path.unlink(missing_ok=True)
+        except BaseException as e:
+            messenger.log_problem(
+                ProblemLevel.WARN,
+                f"Failed to check download '{live_path.as_posix()}: {e}'",
+                traceback.format_exc(),
+            )
+
+    if results[0] is not None:
+        raise Exception("Failed to download the Kids Connection video.") from results[0]
 
 
 _VIDEO_CONTENT_TYPES = {
@@ -159,13 +176,15 @@ _KIDS_VIDEO_FILENAME_REGEX = re.compile(r"^kids.*", flags=re.IGNORECASE)
 _DOCX_CONTENT_TYPE = (
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 )
-_SERMON_NOTES_REGEX = re.compile(r"^notes\s*-.*", flags=re.IGNORECASE)
+_SERMON_NOTES_REGEX = re.compile(r"^notes.*", flags=re.IGNORECASE)
 _IMAGE_CONTENT_TYPES = {"image/bmp", "image/jpeg", "image/png", "image/tiff"}
 
 
 def _classify_attachments(
     attachments: Set[Attachment],
-) -> Tuple[Set[Attachment], Set[Attachment], Set[Attachment], Set[Attachment]]:
+) -> Tuple[
+    Set[Attachment], Set[Attachment], Set[Attachment], Set[Attachment], Set[Attachment]
+]:
     kids_videos = {
         a
         for a in attachments
@@ -187,7 +206,8 @@ def _classify_attachments(
     other_videos = {
         a for a in attachments if a.content_type.lower() in _VIDEO_CONTENT_TYPES
     }
-    return kids_videos, notes, other_images, other_videos
+    attachments -= other_videos
+    return kids_videos, notes, other_images, other_videos, attachments
 
 
 def _any(s: Set[Attachment]) -> Attachment:
