e2399f8d49c27d6cf0c133404c1a19b5e209ba46
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2023-12-19 23:43:48 -0500

    Try to improve spacing in ConsoleMessenger

--- scripts/autochecklist/messenger/console_messenger.py
+++ scripts/autochecklist/messenger/console_messenger.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass, field
 from getpass import getpass
 from queue import Empty, PriorityQueue
 from threading import Event, Lock
-from typing import Callable, Dict, Optional, Set, Tuple, TypeVar
+from typing import Callable, Dict, Iterable, Optional, Set, Tuple, TypeVar
 
 from .input_messenger import (
     InputMessenger,
@@ -32,10 +32,11 @@ class ConsoleMessenger(InputMessenger):
         self._commands: Dict[Tuple[str, str], Callable[[], None]] = {}
         self._waiting_events: Set[Event] = set()
         self._queue: PriorityQueue[_QueueTask] = PriorityQueue()
+        self._io = _IO()
 
     def run_main_loop(self) -> None:
         try:
-            print(f"{self._description}\n\nPress CTRL+C to see the menu.\n\n")
+            self._io.write(f"{self._description}\n\nPress CTRL+C to see the menu.\n")
             should_exit = False
             task: Optional[_QueueTask] = None
             while True:
@@ -68,10 +69,10 @@ class ConsoleMessenger(InputMessenger):
                             self._queue.put(task)
                         self._run_menu()
                     except (KeyboardInterrupt, EOFError):
-                        print("\nProgram cancelled.")
+                        self._io.write("\nProgram cancelled.")
                         return
                 except Exception as e:
-                    print(f"Error while running task from queue: {e}")
+                    self._io.write(f"Error while running task from queue: {e}")
         finally:
             # Release all waiting threads so they can exit cleanly
             for e in self._waiting_events:
@@ -82,10 +83,11 @@ class ConsoleMessenger(InputMessenger):
     def _run_menu(self) -> None:
         command_by_key = self._get_and_display_menu()
         while True:
-            choice = input(">> ")
+            choice = self._io.read(">> ")
             if choice == "q":
                 raise KeyboardInterrupt()
             elif choice == "p":
+                self._io.write()
                 return
             elif choice == "r":
                 command_by_key = self._get_and_display_menu()
@@ -95,22 +97,26 @@ class ConsoleMessenger(InputMessenger):
                         task, command = command_by_key[choice]
                         callback = self._commands[task, command]
                 except KeyError:
-                    print("That command is no longer available.")
+                    self._io.write("That command is no longer available.")
                     continue
                 try:
                     callback()
                 except Exception as e:
-                    print(f"An error occurred: {e}")
+                    self._io.write(f"An error occurred: {e}")
                 command_by_key = self._get_and_display_menu()
+            elif choice.strip() == "":
+                continue
+            else:
+                self._io.write(f"Unknown choice '{choice}'.")
 
     def _get_and_display_menu(self) -> Dict[str, Tuple[str, str]]:
         with self._mutex:
             task_command_pairs = sorted(self._commands.keys())
         command_by_key: Dict[str, Tuple[str, str]] = {}
         count = 1
-        print()
+        self._io.write()
         if len(task_command_pairs) == 0:
-            print("No commands available.")
+            self._io.write("\nNo commands available.")
         else:
             for task, command in task_command_pairs:
                 command_by_key[str(count)] = (task, command)
@@ -122,12 +128,14 @@ class ConsoleMessenger(InputMessenger):
             max_task_len = max(len(task_header), max_task_len)
             max_command_len = max(len(command) for (_, command) in task_command_pairs)
             max_command_len = max(len(command_header), max_command_len)
-            print(
+            self._io.write(
                 f"{task_header:<{max_task_len}} | {command_header:<{max_command_len}} | {key_header}"
             )
             for key, (task, command) in command_by_key.items():
-                print(f"{task:<{max_task_len}} | {command:<{max_command_len}} | {key}")
-        print()
+                self._io.write(
+                    f"{task:<{max_task_len}} | {command:<{max_command_len}} | {key}"
+                )
+        self._io.write()
         instruction_lines = ["Options:"]
         if len(task_command_pairs) > 0:
             instruction_lines.append("- Enter the key for a command listed above.")
@@ -136,7 +144,7 @@ class ConsoleMessenger(InputMessenger):
             "- p: go back to script",
             "- q: quit script",
         ]
-        print("\n".join(instruction_lines))
+        self._io.write("\n".join(instruction_lines))
         return command_by_key
 
     def wait_for_start(self) -> None:
@@ -165,7 +173,7 @@ class ConsoleMessenger(InputMessenger):
         self, task_name: str, index: Optional[int], status: TaskStatus, message: str
     ) -> None:
         def do_log_status() -> None:
-            print(f"({task_name} | {status}) {message}")
+            self._io.write(f"({task_name} | {status}) {message}")
 
         if self.is_closed:
             raise KeyboardInterrupt()
@@ -182,7 +190,7 @@ class ConsoleMessenger(InputMessenger):
 
     def log_problem(self, task_name: str, level: ProblemLevel, message: str) -> None:
         def do_log_problem() -> None:
-            print(f"[{task_name} | {level}] {message}")
+            self._io.write(f"[{task_name} | {level}] {message}")
 
         if self.is_closed:
             raise KeyboardInterrupt()
@@ -216,9 +224,9 @@ class ConsoleMessenger(InputMessenger):
     ) -> Dict[str, object]:
         def read_input() -> None:
             if prompt:
-                print(f"\n{prompt}")
+                self._io.write(f"\n{prompt}")
             for name, param in params.items():
-                input_func = getpass if param.password else input
+                input_func = self._io.read_password if param.password else self._io.read
                 first_attempt = True
                 while True:
                     try:
@@ -228,22 +236,23 @@ class ConsoleMessenger(InputMessenger):
                         message = f"{param.display_name}{default_message}:"
                         if param.description and first_attempt:
                             message += f"\n({param.description})"
-                        print(message)
+                        self._io.write(message)
                         raw_value = input_func(">> ")
                         if not raw_value and param.default:
                             raw_value = param.default
                         output_by_key[name] = param.parser(raw_value)
                         break
                     except ArgumentTypeError as e:
-                        print(f"Invalid input: {e}")
+                        self._io.write(f"Invalid input: {e}")
                     # IMPORTANT: Ignore both KeyboardInterrupt and EOFError so
                     # that the main loop can deal with all the
                     # cancellation-related issues
                     except (KeyboardInterrupt, EOFError):
                         raise
                     except Exception as e:
-                        print(f"An error occurred while taking input: {e}")
+                        self._io.write(f"An error occurred while taking input: {e}")
                     first_attempt = False
+            self._io.write()
             submit_event.set()
 
         output_by_key: Dict[str, object] = {key: "" for key in params}
@@ -265,27 +274,31 @@ class ConsoleMessenger(InputMessenger):
     def input_bool(self, prompt: str, title: str = "") -> bool:
         def read_input() -> None:
             nonlocal choice
-            print(f"\n{prompt} [y/n]")
+            self._io.write(f"\n{prompt} [y/n]")
             while True:
                 try:
-                    result = input(">> ")
+                    result = self._io.read(">> ")
                     if result.lower() in ["y", "yes"]:
                         choice = True
+                        self._io.write()
                         submit_event.set()
                         return
                     elif result.lower() in ["n", "no"]:
                         choice = False
+                        self._io.write()
                         submit_event.set()
                         return
                     else:
-                        print("Invalid input. Enter 'y' for yes or 'n' for no.")
+                        self._io.write(
+                            "Invalid input. Enter 'y' for yes or 'n' for no."
+                        )
                 # IMPORTANT: Ignore both KeyboardInterrupt and EOFError so
                 # that the main loop can deal with all the
                 # cancellation-related issues
                 except (KeyboardInterrupt, EOFError):
                     raise
                 except Exception as e:
-                    print(f"An error occurred while waiting for input: {e}")
+                    self._io.write(f"An error occurred while waiting for input: {e}")
                     continue
 
         submit_event = Event()
@@ -309,22 +322,24 @@ class ConsoleMessenger(InputMessenger):
     ) -> UserResponse:
         def wait_for_input_with_retry() -> None:
             nonlocal response
-            print(
+            self._io.write(
                 f"{prompt} Type 'retry' if you would like to try completing the task automatically again. Type 'done' if you have completed the task manually.",
             )
             while True:
                 try:
-                    choice = input(">> ")
+                    choice = self._io.read(">> ")
                     if choice.lower() == "done":
                         response = UserResponse.DONE
+                        self._io.write()
                         input_received_event.set()
                         return
                     elif choice.lower() == "retry":
                         response = UserResponse.RETRY
+                        self._io.write()
                         input_received_event.set()
                         return
                     else:
-                        print(
+                        self._io.write(
                             "Invalid choice. Type 'retry' if you would like to try completing the task automatically again. Type 'done' if you have completed the task manually."
                         )
                         continue
@@ -334,7 +349,7 @@ class ConsoleMessenger(InputMessenger):
                 except (KeyboardInterrupt, EOFError):
                     raise
                 except Exception as e:
-                    print(
+                    self._io.write(
                         f"An error occurred while waiting for input. Assuming the task was completed manually. {e}"
                     )
                     response = UserResponse.DONE
@@ -346,8 +361,9 @@ class ConsoleMessenger(InputMessenger):
             try:
                 # Ask for a password so that pressing keys other than
                 # ENTER has no visible effect
-                getpass(f"{prompt} Press ENTER when you're done.\n>> ")
+                self._io.read_password(f"{prompt} Press ENTER when you're done.\n>> ")
                 response = UserResponse.DONE
+                self._io.write()
                 input_received_event.set()
             # IMPORTANT: Ignore both KeyboardInterrupt and EOFError so
             # that the main loop can deal with all the
@@ -355,8 +371,8 @@ class ConsoleMessenger(InputMessenger):
             except (KeyboardInterrupt, EOFError):
                 raise
             except Exception as e:
-                print(
-                    f"An error occurred while waiting for input. Assuming the task was completed manually. {e}"
+                self._io.write(
+                    f"An error occurred while waiting for input. Assuming the task was completed manually. {e}\n"
                 )
                 response = UserResponse.DONE
                 input_received_event.set()
@@ -410,6 +426,68 @@ class ConsoleMessenger(InputMessenger):
             self._waiting_events.remove(event)
         if self.is_closed:
             raise KeyboardInterrupt()
+
+
+class _IO:
+    """
+    Handles printing to the console in such a way that the number of blank
+    lines between lines of text is consistent regardless of situation.
+    """
+
+    def __init__(self) -> None:
+        self._mutex = Lock()
+        self._current_blank_lines = 0
+
+    def write(self, text: str = "") -> None:
+        if text.strip() == "":
+            with self._mutex:
+                print()
+                self._current_blank_lines += 1
+        else:
+            lines = text.split("\n")
+            num_leading = self._count_leading_blanks(lines)
+            num_trailing = self._count_leading_blanks(reversed(lines))
+            with self._mutex:
+                k = max(0, num_leading - self._current_blank_lines)
+                text_to_print = k * "\n" + text.lstrip()
+                print(text_to_print)
+                self._current_blank_lines = num_trailing
+
+    def read(self, prompt: str = "") -> str:
+        return self._read(prompt, input)
+
+    def read_password(self, prompt: str = "") -> str:
+        return self._read(prompt, getpass)
+
+    def _read(self, prompt: str, input_func: Callable[[str], str]) -> str:
+        if prompt.strip() == "":
+            with self._mutex:
+                val = input_func(prompt)
+                # The user needs to press ENTER to submit, which adds a newline
+                self._current_blank_lines += 1
+                return val
+        else:
+            lines = prompt.split("\n")
+            num_leading = self._count_leading_blanks(lines)
+            # Subtract one because no trailing newline is added
+            num_trailing = self._count_leading_blanks(reversed(lines)) - 1
+            with self._mutex:
+                k = max(0, num_leading - self._current_blank_lines)
+                text_to_print = k * "\n" + prompt.lstrip()
+                self._current_blank_lines = num_trailing
+                val = input_func(text_to_print)
+                # The user needs to press ENTER to submit, which adds a newline
+                self._current_blank_lines += 1
+                return val
+
+    def _count_leading_blanks(self, lines: Iterable[str]) -> int:
+        num_leading = 0
+        for l in lines:
+            if not l.strip():
+                num_leading += 1
+            else:
+                break
+        return num_leading
 
 
 @dataclass(frozen=True, order=True)
