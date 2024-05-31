61fa116f65d07e5c14608784aa2d7918a4a55f5c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-05-30 22:43:26 -0400

    Make script cross-platform

--- scripts/test/integration/test_undo_updates.py
+++ scripts/test/integration/test_undo_updates.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import shutil
 import subprocess
 import unittest
@@ -7,13 +8,16 @@ from typing import List
 
 GIT_REPO_URL = "https://github.com/recc-tech/tech.git"
 OUTER_REPO_ROOT = Path(__file__).resolve().parent.parent.parent.parent
-OUTER_UNDO_UPDATES_SCRIPT = OUTER_REPO_ROOT.joinpath("scripts", "undo_updates.command")
+OUTER_UNDO_UPDATES_NIX = OUTER_REPO_ROOT.joinpath("scripts", "undo_updates.command")
+OUTER_UNDO_UPDATES_WINDOWS = OUTER_REPO_ROOT.joinpath("scripts", "undo_updates.bat")
+OUTER_UNDO_UPDATES_PY = OUTER_REPO_ROOT.joinpath("scripts", "undo_updates.py")
 INNER_REPO_ROOT = Path(__file__).resolve().parent.joinpath("repo_copy")
-INNER_UNDO_UPDATES_SCRIPT = INNER_REPO_ROOT.joinpath("scripts", "undo_updates.command")
+INNER_UNDO_UPDATES_NIX = INNER_REPO_ROOT.joinpath("scripts", "undo_updates.command")
+INNER_UNDO_UPDATES_WINDOWS = INNER_REPO_ROOT.joinpath("scripts", "undo_updates.bat")
+INNER_UNDO_UPDATES_PY = INNER_REPO_ROOT.joinpath("scripts", "undo_updates.py")
 
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
-YELLOW = "\033[0;33m"
 RESET_COLOR = "\033[0m"
 
 
@@ -29,15 +33,34 @@ class UndoUpdatesTestCase(unittest.TestCase):
         # Test on a copy of the repo because checking out random commits in the
         # current repo while running tests would be very wonky.
         subprocess.run(["git", "clone", GIT_REPO_URL, INNER_REPO_ROOT])
-        os.chdir(INNER_REPO_ROOT)
         cls.avail_tags = sorted(_get_tags(), reverse=True)[:5]
+        cls.maxDiff = None
+        cls.original_cwd = os.getcwd()
+        os.chdir(INNER_REPO_ROOT)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        os.chdir(cls.original_cwd)
+        shutil.rmtree(INNER_REPO_ROOT, ignore_errors=True)
 
     def setUp(self) -> None:
-        shutil.copy(src=OUTER_UNDO_UPDATES_SCRIPT, dst=INNER_UNDO_UPDATES_SCRIPT)
+        current_os = platform.system().lower().strip()
+        if current_os in {"macos", "linux"}:
+            self.outer_undo_updates_script = OUTER_UNDO_UPDATES_NIX
+            self.inner_undo_updates_script = INNER_UNDO_UPDATES_NIX
+        elif current_os == "windows":
+            self.outer_undo_updates_script = OUTER_UNDO_UPDATES_WINDOWS
+            self.inner_undo_updates_script = INNER_UNDO_UPDATES_WINDOWS
+        else:
+            self.fail(f"Operating system '{current_os}' is not supported.")
+        shutil.copy(
+            src=self.outer_undo_updates_script, dst=self.inner_undo_updates_script
+        )
+        shutil.copy(src=OUTER_UNDO_UPDATES_PY, dst=INNER_UNDO_UPDATES_PY)
 
     def test_rollback_to_latest(self) -> None:
         result = subprocess.run(
-            [INNER_UNDO_UPDATES_SCRIPT.as_posix()],
+            [self.inner_undo_updates_script.as_posix()],
             input="latest\n\n",
             encoding="ascii",
             capture_output=True,
@@ -57,7 +80,7 @@ Press ENTER to exit..."""
     def test_rollback_to_second_latest(self) -> None:
         tag = self.avail_tags[1]
         result = subprocess.run(
-            [INNER_UNDO_UPDATES_SCRIPT.as_posix()],
+            [self.inner_undo_updates_script.as_posix()],
             input=f"{tag}\n\n",
             encoding="ascii",
             capture_output=True,
@@ -76,23 +99,22 @@ Press ENTER to exit..."""
 
     def test_rollback_to_invalid(self) -> None:
         result = subprocess.run(
-            [INNER_UNDO_UPDATES_SCRIPT.as_posix()],
+            [self.inner_undo_updates_script.as_posix()],
             input=f"\n\n",
             encoding="ascii",
             capture_output=True,
         )
         tag_list = "\n".join([f" * {t}" for t in self.avail_tags])
-        expected = f"""AVAILABLE VERSIONS:
+        expected_stdout = f"""AVAILABLE VERSIONS:
  * latest
 {tag_list}
 
 SELECT THE VERSION TO USE:
-> {RED}Failed to switch to the selected version. Are you sure you selected a valid one?{RESET_COLOR}
-{YELLOW}error: pathspec 'tags/' did not match any file(s) known to git{RESET_COLOR}
-Press ENTER to exit..."""
-        self.assertEqual(0, result.returncode)
-        self.assertEqual(expected, result.stdout)
-        self.assertEqual("", result.stderr)
+> Press ENTER to exit..."""
+        expected_stderr = f"{RED}Invalid choice.{RESET_COLOR}\n"
+        self.assertEqual(255, result.returncode)
+        self.assertEqual(expected_stdout, result.stdout)
+        self.assertEqual(expected_stderr, result.stderr)
 
 
 def _get_tags() -> List[str]:
--- scripts/undo_updates.command
+++ scripts/undo_updates.command
@@ -1,52 +1,6 @@
 #!/bin/bash
 
-set -u
+# Go to the scripts directory
+cd "$(dirname "$0")"
 
-LATEST="latest"
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-RESET_COLOR='\033[0m'
-
-function undo_updates {
-	echo "AVAILABLE VERSIONS:"
-	echo " * latest"
-	git tag --list | sort --reverse | head -n 5 | sed -rn 's/(.*)/ * \1/p'
-
-	echo
-	echo "SELECT THE VERSION TO USE:"
-	echo -n "> "
-	read -r tag
-
-	if [[ "$tag" = "latest" ]]; then
-		gitmsg="$( git switch main 2>&1 )"
-		if [[ "$?" != "0" ]]; then
-			echo -e "${RED}Failed to switch to the latest version due to an unexpected error.${RESET_COLOR}"
-			echo -e "${YELLOW}${gitmsg}${RESET_COLOR}"
-		else
-			echo -e "${GREEN}OK: currently on the latest version.${RESET_COLOR}"
-		fi
-		return
-	fi
-
-	gitmsg="$( git checkout "tags/$tag" 2>&1 )"
-	if [[ "$?" != "0" ]]; then
-		echo -e "${RED}Failed to switch to the selected version. Are you sure you selected a valid one?${RESET_COLOR}"
-		echo -e "${YELLOW}${gitmsg}${RESET_COLOR}"
-		return
-	fi
-
-	current_tag="$( git describe 2>&1 )"
-	if [[ "$?" != "0" ]]; then
-		echo -e "${RED}Switching versions seemed to work, but now I can't tell which version we're currently on...${RESET_COLOR}"
-		echo -e "${YELLOW}${current_tag}${RESET_COLOR}"
-		return
-	fi
-
-	echo -e "${GREEN}OK: currently on version ${current_tag}.${RESET_COLOR}"
-}
-
-undo_updates
-echo -n "Press ENTER to exit..."
-read -r _
+python3 undo_updates.py
