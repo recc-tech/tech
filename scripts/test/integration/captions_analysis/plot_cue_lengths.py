dcae73bc3f02c6758cfbc82dd439adac5d3e1255
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-07-11 16:33:39 -0400

    Ignore type issue due to new matplotlib version

--- scripts/test/integration/captions_analysis/plot_cue_lengths.py
+++ scripts/test/integration/captions_analysis/plot_cue_lengths.py
@@ -33,7 +33,9 @@ def main():
         for i in range(len(original_vtt))
     }
 
-    fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1)
+    fig, (ax0, ax1) = plt.subplots(
+        nrows=2, ncols=1
+    )  # pyright: ignore[reportGeneralTypeIssues]
 
     ax0.bar(
         list(range(len(original_vtt))),
