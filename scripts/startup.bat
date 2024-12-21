47088362c74ef19707247656056f0ce5bf062f90
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-12-20 20:21:52 -0500

    Fix Windows startup script

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -4,6 +4,6 @@
 :: You need to pass the /D flag because the MCR computer uses the D:/ drive.
 CD /D %~dp0
 
-./update_scripts.bat
-./launch_apps.bat --auto-close
-./mcr_setup.bat
+CALL ./update_scripts.bat
+CALL ./launch_apps.bat --auto-close
+CALL ./mcr_setup.bat
