dfbd206fe712082cb2ef5eec41778186f62c333b
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-02-15 23:25:42 -0500

    Launch PCO live view in fullscreen

--- scripts/external_services/local_apps.py
+++ scripts/external_services/local_apps.py
@@ -6,9 +6,13 @@ import subprocess
 from pathlib import Path
 
 
-def launch_firefox(url: str) -> None:
+def launch_firefox(url: str, fullscreen: bool = False) -> None:
+    if fullscreen:
+        args = ["firefox", "--new-window", url, "--kiosk"]
+    else:
+        args = ["firefox", url]
     # Use Popen so this doesn't block
-    subprocess.Popen(["firefox", url])
+    subprocess.Popen(args)
 
 
 def launch_vmix(preset: Path) -> None:
