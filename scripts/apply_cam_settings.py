e10e56130aa880a585e71b8e66316381660192b7
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2026-02-12 08:20:15 -0500

    Integrate apply_cam_settings.py into rest of project

--- scripts/apply_cam_settings.py
+++ scripts/apply_cam_settings.py
@@ -1,31 +1,43 @@
-import requests
-from pathlib import Path
+import sys
+from typing import List, Literal
+
+import autochecklist
+from args import ReccArgs
+from autochecklist import Messenger, TaskModel, TaskStatus
+from config import Config
+from external_services import Credential, CredentialStore, InputPolicy
+from lib import ReccDependencyProvider, SimplifiedMessengerSettings
+from requests import Session
 
 
-# cam-centre: http://192.168.0.100/videoset
-# cam-left: http://192.168.0.101/videoset
-# cam-right: http://192.168.0.102/videoset
-BOUNDARY = "----recctechformboundary"
-BASE_URL = {
-    "cam_1": "http://192.168.0.100",
-    "cam_2": "http://192.168.0.102",
-    "cam_3": "http://192.168.0.101",
-}
-CAM_SETTINGS_DIR = Path(__file__).parent / "config" / "cameras"
-SETTINGS = {
-    "cam_1": (CAM_SETTINGS_DIR / "cam_1.txt").read_text(),
-    "cam_2": (CAM_SETTINGS_DIR / "cam_2.txt").read_text(),
-    "cam_3": (CAM_SETTINGS_DIR / "cam_3.txt").read_text(),
-}
+class ApplyCamSettingsArgs(ReccArgs):
+    NAME = "apply_cam_settings"
+    DESCRIPTION = (
+        "This script will apply the stored settings to each BirdDog PTZ camera,"
+        " allowing quicker setup after power interruptions."
+    )
 
 
-def main() -> None:
-    password = input("BirdDog password: ")
-    for cam in ["cam_1", "cam_2", "cam_3"]:
-        print(f"Setting up {cam.replace('_', ' ')}")
-        with requests.Session() as s:
+def _make_task(
+    camera: Literal[1, 2, 3],
+    config: Config,
+    messenger: Messenger,
+    credential_store: CredentialStore,
+):
+    def apply_cam_settings() -> None:
+        password = credential_store.get(
+            Credential.BIRD_DOG_PASSWORD,
+            request_input=InputPolicy.AS_REQUIRED,
+        )
+        base_url = config.cam_base_url[camera]
+        settings_path = config.cam_settings_path[camera]
+        settings = settings_path.read_text()
+        with Session() as s:
+            messenger.log_status(TaskStatus.RUNNING, "Logging in...")
+            url = f"{base_url}/login"
+            messenger.log_debug(f"Sending login request to {url}")
             s.post(
-                f"{BASE_URL[cam]}/login",
+                url,
                 data=f"auth_password={password}",
                 headers={
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -34,25 +46,70 @@ def main() -> None:
                     "Connection": "keep-alive",
                     "Content-Type": "application/x-www-form-urlencoded",
                     "Cookie": "mod_sel=none; av_settings=none; exp_settings=block; wb_settings=none; pic1_settings=none; pic2_settings=none; cm_settings=none; ci_settings=none; cex_settings=none",
-                    "Host": BASE_URL[cam][len("http://"):],
-                    "Origin": BASE_URL[cam],
+                    "Host": base_url[len("http://") :],
+                    "Origin": base_url,
                     "Priority": "u=0, i",
-                    "Referer": f"{BASE_URL[cam]}/login",
+                    "Referer": f"{base_url}/login",
                     "Upgrade-Insecure-Requests": "1",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0",
-                }
-            )
-            response = s.post(
-                f"{BASE_URL[cam]}/videoset",
-                data=SETTINGS[cam],
-                headers={
-                    "Content-Type": f"multipart/form-data; boundary={BOUNDARY}",
                 },
-                cookies={
-                    "BirdDogSession": "tOldLgBtXZSunYNCqerDsdKXyIjvNMXWQYVPrkfACiYiyhrvhFBkqvEoFAGqVAXFNuDLKWtJtJEIavaHRupJetxPUgJWCTVqjzHf"
-                }
             )
+            messenger.log_status(
+                TaskStatus.RUNNING,
+                f"Sending settings (from {settings_path})...",
+            )
+            s.post(
+                f"{base_url}/videoset",
+                data=settings,
+                headers={
+                    "Content-Type": f"multipart/form-data; boundary={config.cam_settings_form_boundary}",
+                },
+            )
+
+    return apply_cam_settings
+
+
+def main(
+    args: ReccArgs,
+    config: Config,
+    dep: ReccDependencyProvider,
+) -> None:
+    all_cameras: List[Literal[1, 2, 3]] = [1, 2, 3]
+    tasks = TaskModel(
+        name="apply_cam_settings",
+        subtasks=[
+            TaskModel(
+                name=f"setup_cam_{camera}",
+                description=f"Failed to apply settings for camera {camera}",
+                only_auto=True,
+                func=_make_task(
+                    camera=camera,
+                    config=config,
+                    messenger=dep.messenger,
+                    credential_store=dep.get(CredentialStore),
+                ),
+            )
+            for camera in all_cameras
+        ],
+    )
+    autochecklist.run(
+        args=args,
+        config=config,
+        dependency_provider=dep,
+        tasks=tasks,
+        module=sys.modules[__name__],
+    )
 
 
 if __name__ == "__main__":
-    main()
+    args = ApplyCamSettingsArgs.parse(sys.argv)
+    config = Config(args)
+    msg = SimplifiedMessengerSettings(
+        log_file=config.apply_cam_settings_log,
+        script_name="Apply Camera Settings",
+        description=ApplyCamSettingsArgs.DESCRIPTION,
+        show_statuses_by_default=True,
+    )
+    dependency_provider = ReccDependencyProvider(
+        args=args, config=config, messenger=msg
+    )
+    main(args, config, dependency_provider)
